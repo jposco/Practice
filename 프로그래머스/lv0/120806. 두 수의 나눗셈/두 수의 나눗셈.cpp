e91b072f84f855efed2266cf013f338adca6ef29
jposco <88217259+jposco@users.noreply.github.com>
2023-05-25 12:14:03 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    double tmp;
+    tmp = ((double)num1/num2)*1000;
+answer=tmp;
+    return answer;
+}
