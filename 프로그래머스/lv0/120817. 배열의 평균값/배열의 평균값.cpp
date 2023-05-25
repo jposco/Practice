96ac5e3d279ae0d402042a72592b727db45b3ec0
jposco <88217259+jposco@users.noreply.github.com>
2023-05-25 12:27:24 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    for(auto i : numbers)
+    {
+        answer+=i;
+    }
+    return answer/numbers.size();
+}
