704bd540d082296bf57433f8318d13dbf6ac1c46
jposco <88217259+jposco@users.noreply.github.com>
2023-05-27 19:19:13 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -1,16 +1,9 @@
-#include <string>
-#include <vector>
-
+#include <cmath>
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    int x=0;
-    int y=0;
-    x = n/7; //2
-    y= n%7; //1
-    if(n<8) return 1;
-    if(y>0) answer=x+1;
-    else answer = x   ; 
+    double x = n/7.0;
+    answer = ceil(x);
     return answer;
 }
