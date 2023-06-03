eb53630b6da03f9a2b93f492d176013f32798c90
jposco <88217259+jposco@users.noreply.github.com>
2023-06-03 10:26:39 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,65 +1,21 @@
+#include <algorithm>
+#include <regex>
 #include <string>
-#include <vector>
 
 using namespace std;
 
+vector<string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
 int solution(string s) {
     int answer = 0;
-    string start = "";
-    for (int i=0;i<s.size();i++)
-    {
-        if (s[i] == 'o') {
-            i += 2;
-            start += '1';
-        }
-        else if (s[i] == 't' && s[i + 1] == 'w')
-        {
-            i += 2;
-            start += '2';
-        }
-        else if (s[i] == 't' && s[i + 1] == 'h')
-        {
-            i += 4;
-            start += '3';
-        }
-        else if (s[i] == 'f' && s[i + 1] == 'o')
-        {
-            i += 3;
-            start += '4';
-        }
-        else if (s[i] == 'f' && s[i + 1] == 'i')
-        {
-            i += 3;
-            start += '5';
-        }
-
-        else if (s[i] == 's' && s[i + 1] == 'i')
-        {
-            i += 2;
-            start += '6';
-        }
-        else if (s[i] == 's' && s[i + 1] == 'e')
-        {
-            i += 4;
-            start += '7';
-        }
-        else if (s[i] == 'e')
-        {
-            i += 4;
-            start += '8';
-        }
-        else if (s[i] == 'n')
-        {
-            i += 3;
-            start += '9';
-        }
-        else if(isdigit(s[i])) start += s[i];
-        else
-        {
-            i += 3;
-            start += '0';
-        }        
+    for(int i = 0; i < numbers.size(); i++) {
+        string word = numbers[i];
+        size_t found = s.find(word);
+        while (found != string::npos) {
+            s.replace(found, word.length(), to_string(i));
+            found = s.find(word, found + 1);
     }
-    answer = stoi(start);
+    }
+    answer = stoi(s);
     return answer;
 }
